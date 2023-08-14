2c228d2761e5c0f82c329bb991326fd57ce05a20
i-moonlight <infinite.moonlight0506@gmail.com>
2023-08-13 21:07:39 -0300

    [Backend] Adicionando paginação

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ProductCatalog.Entities;
 using ProductCatalog.Enums;
 using ProductCatalog.Models;
@@ -11,6 +12,7 @@ public sealed class ProductController : ControllerBase
 {
     private readonly ProductCatalogDbContext _context;
     private readonly ILogger<ProductController> _logger;
+    private const int PageSize = 5;
     
     public ProductController(
         ProductCatalogDbContext context, 
@@ -21,21 +23,24 @@ public sealed class ProductController : ControllerBase
     }
     
     [HttpGet]
-    public IActionResult Get()
+    public async Task<IActionResult> Get(int pageIndex = 1)
     {
-        var products = _context.Products.ToList();
+        var products = await _context.Products
+            .OrderBy(p => p.Name)
+            .Skip((pageIndex - 1) * PageSize)
+            .Take(PageSize)
+            .ToListAsync();
         return Ok(products);
     }
     
     [HttpPost]
     public IActionResult Post(ProductModel productModel)
     {
-
         if (_context.Products
                 .Any(p => p.Name.ToLower().Trim() == productModel.Name.ToLower().Trim()))
         {
-            _logger.LogError("Product already exists");
-            return BadRequest("Product already exists");
+            _logger.LogError("O produto já existe.");
+            return BadRequest("O produto já existe.");
         }
         
         var product = new Product()
