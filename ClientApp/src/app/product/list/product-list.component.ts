34e246aecca50a9687502b52681828815db32ca1
i-moonlight <infinite.moonlight0506@gmail.com>
2023-08-14 22:50:05 -0300

    Adicionando paginação

--- ClientApp/src/app/product/list/product-list.component.ts
+++ ClientApp/src/app/product/list/product-list.component.ts
@@ -7,13 +7,41 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ProductListComponent {
   public products: Product[] = [];
-
+  public pageIndex = 1;
+  public totalPages: number = 1;
+  private _baseUrl: string;
+  private _http: HttpClient;
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<Product[]>(baseUrl + 'product')
+    this._http = http;
+    this._baseUrl = baseUrl;
+  }
+
+  ngOnInit() {
+    this.fetchProducts(this.pageIndex);
+  }
+
+  public pagePrev(){
+    if(this.pageIndex > 1){
+      this.pageIndex = this.pageIndex - 1;
+      this.fetchProducts(this.pageIndex);
+    }
+  }
+
+  public pageNext(){
+    if(this.pageIndex < this.totalPages){
+      this.pageIndex = this.pageIndex + 1;
+      this.fetchProducts(this.pageIndex);
+    }
+  }
+
+  fetchProducts(pageIndex:number) {
+    this._http.get<Product[]>(this._baseUrl + 'product?pageIndex=' + this.pageIndex, { observe: 'response' })
       .subscribe((data) => {
-        this.products = data;
+        this.totalPages = Number(data.headers.get('totalPages'));
+        this.products = data.body as Product[];
       });
   }
+
 }
 
 interface Product {
