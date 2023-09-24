bd4488eea731ab98b1fd1904cac494a4aeae8219
i-moonlight <infinite.moonlight0506@gmail.com>
2023-09-24 11:08:43 -0300

    refactoring/movendo da raiz para dentro da pasta side-nav
    O arquivo que contém os items a serem exibidos na sidenav está sendo movido da raiz do projeto para a pasta /side-nav.

--- ClientApp/src/app/side-nav/side-nav.component.ts
+++ ClientApp/src/app/side-nav/side-nav.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {sidenavItems} from "../sidenav-items";
+import {sidenavItems} from "./sidenav-items";
 
 @Component({
   selector: 'app-side-nav',
