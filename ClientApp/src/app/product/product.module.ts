113de6ef9e473b4a0ee535ff268745131ba7e944
Eugenio Lopes Fernandes Lima <eugeniolopesfernandeslima1997@outlook.com>
2023-09-23 21:19:18 -0300

    Adaptando o layout de tabela para grid
    Com essa mudança os produtos serão exibidos em cards.

--- ClientApp/src/app/product/product.module.ts
+++ ClientApp/src/app/product/product.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
 import { NgxMaskModule } from 'ngx-mask';
 
 import { ProductListComponent } from "./list/product-list.component";
 import {RegisterProductComponent} from "./register/register-product.component";
 import {ReactiveFormsModule} from "@angular/forms";
+import {ProductService} from "./service/product.service";
 
 @NgModule({
   declarations: [
@@ -15,10 +16,14 @@ import {ReactiveFormsModule} from "@angular/forms";
     CommonModule,
     ReactiveFormsModule,
     NgxMaskModule.forRoot(),
+    NgOptimizedImage,
   ],
   exports: [
     ProductListComponent,
     RegisterProductComponent
-  ]
+  ],
+  providers: [
+    ProductService
+  ],
 })
 export class ProductModule { }
