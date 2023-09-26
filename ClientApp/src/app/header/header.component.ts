65b8efd827c8503b34d61437fe4e8d95eed757e3
Eugenio Lopes Fernandes Lima <eugeniolimaeugenio5000@outlook.com>
2023-09-25 21:22:24 -0300

    Adicionando o abrir e collapsar dinâmico da sidenav
    A sidenav vai abrir e colapsar dinamicamente de acordo com o width da janela.
    
    Breakpoints:
    -   <=768px a sidenav vai colapsar,
    -   >768 a sidenav vai abrir

--- ClientApp/src/app/header/header.component.ts
+++ ClientApp/src/app/header/header.component.ts
@@ -1,21 +1,40 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, Output, OnInit, HostListener} from '@angular/core';
+import {ScreenDimensionsEnum} from "../enums/screen-dimensions";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
   //properties
-  public menuStatus: boolean = false;
+  public menuStatus: boolean = true;
+  public screenWidth: number = 0;
 
   //Event Emitters
   @Output() sideNavToggled: EventEmitter<boolean> = new EventEmitter<boolean>(true);
 
+  //hooks
+  ngOnInit(): void {
+    //setting sidenav initial state to open
+    this.sideNavToggled.emit(this.menuStatus);
+  }
+
+  //methods
   public SideNavToggle(): void {
     this.menuStatus = !this.menuStatus;
     this.sideNavToggled.emit(this.menuStatus);
   }
 
+  //Angular built-in decorator to listen to event and handle method
+  @HostListener('window:resize')
+  onResize(): void{
+    //innerWidth returns the width of the window's layout viewport
+    this.screenWidth = window.innerWidth;
+    if(this.screenWidth <= ScreenDimensionsEnum.tablet)
+      this.sideNavToggled.emit(false);
+    else if(this.screenWidth > ScreenDimensionsEnum.laptop)
+      this.sideNavToggled.emit(true);
+  }
 }
