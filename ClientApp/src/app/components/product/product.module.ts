267262e463aa990b16a4f11c25b561ee54f04f53
Eugenio Lopes Fernandes Lima <eugeniolimaeugenio5000@outlook.com>
2023-09-30 23:33:30 -0300

    refactoring/ ajustes gerais

--- ClientApp/src/app/components/product/product.module.ts
+++ ClientApp/src/app/components/product/product.module.ts
@@ -5,11 +5,13 @@ import { NgxMaskModule } from 'ngx-mask';
 import { ProductListComponent } from "./list/product-list.component";
 import {ReactiveFormsModule} from "@angular/forms";
 import { ProductCardComponent } from './product-card/product-card.component';
+import {RegisterProductComponent} from "./register/register-product.component";
 
 @NgModule({
   declarations: [
     ProductListComponent,
     ProductCardComponent,
+    RegisterProductComponent
   ],
     imports: [
         CommonModule,
