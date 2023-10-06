e98d74812b84f41c3d7613b2e1b307ea81dd9659
Eugenio Lopes Fernandes Lima <eugeniolimaeugenio5000@outlook.com>
2023-10-06 03:24:31 -0300

    Adicionando logica de leitura de connectionString a depender da plataforma
    As plataformas são:
     - Local (Utilizando containers)
     - Railway (Onde é feito o deploy)
    
    Railway
     - A condição para ler a connectionString da Railway é no caso da variável de ambiente PORT ter sido obtida.

--- DependencyInjection.cs
+++ DependencyInjection.cs
@@ -6,10 +6,29 @@ public static class DependencyInjection
 {
     public static IServiceCollection ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration)
     {
+        
         services.AddDbContext<ProductCatalogDbContext>(
             options => options.UseNpgsql(
-                configuration.GetConnectionString("ProductCatalogConnectionOnRailway")!));
-
+                IsPlatformRailwayOrLocal(configuration).Result
+            ));
+        
         return services;
     }
+
+    private static Task<string> IsPlatformRailwayOrLocal(IConfiguration configuration)
+    {
+        var portVar = Environment.GetEnvironmentVariable("PORT");
+        
+        if (string.IsNullOrEmpty(portVar))
+        {
+            var onLocally = configuration
+                .GetConnectionString("ProductCatalogConnection");
+            return Task.FromResult(onLocally!);
+        }
+        var onRailway = configuration
+            .GetConnectionString("ProductCatalogConnectionOnRailway");
+    
+        return Task.FromResult(onRailway!);
+    }
+    
 }
