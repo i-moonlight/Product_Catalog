ce23dfb420ad5ee024f3fa22d7725964283b1ba7
i-moonlight <infinite.moonlight0506@gmail.com>
2023-10-07 02:04:00 -0300

    Adicionando logica para ler variavel de ambiente DATABASE_URL e a partir dela construir ConnectionString

--- DependencyInjection.cs
+++ DependencyInjection.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Npgsql;
 
 namespace ProductCatalog;
 
@@ -9,10 +10,36 @@ public static class DependencyInjection
         
         services.AddDbContext<ProductCatalogDbContext>(
             options => options.UseNpgsql(
-                configuration.GetConnectionString("DefaultConnection")
+                GetConnectionString(configuration)
             ));
         
         return services;
+    }
+    
+    private static string GetConnectionString(IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+        return string.IsNullOrEmpty(databaseUrl) 
+            ? connectionString! 
+            : BuildConnectionStringFromUrl(databaseUrl);
+    }
+        
+    private static string BuildConnectionStringFromUrl(string databaseUrl)
+    {
+        var databaseUri = new Uri(databaseUrl);
+        var userInfo = databaseUri.UserInfo.Split(":");
+        var builder = new NpgsqlConnectionStringBuilder()
+        {
+            Host = databaseUri.Host,
+            Port = databaseUri.Port,
+            Username = userInfo[0],
+            Password = userInfo[1],
+            Database = databaseUri.LocalPath.TrimStart('/'),
+            SslMode = SslMode.Require,
+            TrustServerCertificate = true
+        };
+        return builder.ToString();
     }
     
 }
