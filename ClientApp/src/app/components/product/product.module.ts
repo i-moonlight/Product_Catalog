76bd691d8cd682c7f2f1d84e0a5b08bb3aad5287
i-moonlight <infinite.moonlight0506@gmail.com>
2023-10-09 13:38:18 -0300

    refactoring/corrigindo-o-mapeamento-de-paths-no-tsconfig-e-os-imports-gerais
    Os imports gerais é relativo ao import feito em cada arquivo.

--- ClientApp/src/app/components/product/product.module.ts
+++ ClientApp/src/app/components/product/product.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import {CommonModule, NgOptimizedImage} from '@angular/common';
 import { NgxMaskModule } from 'ngx-mask';
 
-import { ProductListComponent } from "@components/product/list";
+import { ProductListComponent } from "@components/product/list/product-list.component";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import { ProductCardComponent } from '@components/product/card';
-import {RegisterProductComponent} from "@components/product/register";
-import {SharedModule} from "../shared/shared.module";
-import {ProductService} from "@service/product";
+import {RegisterProductComponent} from "@components/product/register/register-product.component";
+
+import { ProductCardComponent } from '@components/product/product-card/product-card.component';
+import {SharedModule} from "@shared/shared.module";
+import {ProductService} from "./product.service";
 
 @NgModule({
   declarations: [
