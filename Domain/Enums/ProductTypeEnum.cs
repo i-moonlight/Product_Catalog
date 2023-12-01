f9880b691cdfef0eda808f035a0e119e1754c00f
i-moonlight <infinite.moonlight0506@gmail.com>
2023-12-01 00:11:11 -0300

    feature/#86 [Backend] Adicionando ProductTypeEnum para definir as categorias de produtos

--- Domain/Enums/ProductTypeEnum.cs
+++ Domain/Enums/ProductTypeEnum.cs
@@ -2,6 +2,15 @@ namespace Domain.Enums;
 
 public enum ProductTypeEnum
 {
-    Organic = 0,
-    NonOrganic = 1
+    Watches = 0,
+    StationeryShop = 1,
+    BeautyAndPersonalCare = 2,
+    Automotive = 3,
+    Toys = 4,
+    SmartHome = 5,
+    BedTableAndBath = 6,
+    Games = 7,
+    Books = 8,
+    Computing = 9,
+    Smartphones = 10
 }
