572bf6e358dce5e57054606aae4350e02174feb0
i-moonlight <infinite.moonlight0506@gmail.com>
2023-12-19 16:30:22 -0300

    feature/#88 [Backend] Adicionando migrations e mapeamento na classe de contexto

--- Infrastructure/Migrations/20231219163143_Add-Tables-User_Role.Designer.cs
+++ Infrastructure/Migrations/20231219163143_Add-Tables-User_Role.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(ProductCatalogDbContext))]
-    partial class ProductCatalogDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231219163143_Add-Tables-User_Role")]
+    partial class AddTablesUser_Role
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
