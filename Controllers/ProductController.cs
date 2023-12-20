f661fcad3d5c83c95652fa82069e84373de5e750
i-moonlight <infinite.moonlight0506@gmail.com>
2023-12-20 20:33:22 -0300

    refactoring/#95 [Backend] removendo padrao repositorio aplicado a api produtos

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 using Domain.Entities;
 using Domain.Enums;
-using Domain.Repository;
+using Infrastructure.Context;
+using Infrastructure.Context.Abstraction;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ProductCatalog.Models;
@@ -12,32 +13,37 @@ namespace ProductCatalog.Controllers;
 public sealed class ProductController : ControllerBase
 {
     private readonly ILogger<ProductController> _logger;
-    private readonly IProductRepository _productRepository;
+    private readonly IProductCatalogDbContext _context;
     private const int PageSize = 9;
     
     public ProductController(
         ILogger<ProductController> logger,
-        IProductRepository productRepository)
+        IProductCatalogDbContext context
+        )
     { 
         _logger = logger;
-        _productRepository = productRepository;
+        _context = context;
+        
     }
     
     [HttpGet]
-    public Task<IActionResult> Get(int pageIndex)
+    public async Task<IActionResult> Get(int pageIndex)
     {
-        var totalCount = _productRepository.GetAllProducts().Result.Count;
+        
+        var totalCount = _context.Set<Product>().ToList().Count;
         var totalPages = (int)Math.Ceiling(totalCount / (double)PageSize);
         HttpContext.Response.Headers.Add("totalPages",totalPages.ToString());
         HttpContext.Response.Headers.Add("totalCount",totalCount.ToString());
 
-
-
-        var paginatedList =  _productRepository
-            .GetAllPaginatedProducts(pageIndex,PageSize);
+        var paginatedList = await _context
+            .Set<Product>()
+            .OrderBy(p => p.CreatedAt)
+            .Skip((pageIndex - 1) * PageSize)
+            .Take(PageSize)
+            .ToListAsync();
         
         _logger.LogInformation("Produtos retornados com sucesso.");
-        return Task.FromResult<IActionResult>(Ok(paginatedList.Result));
+        return Ok(paginatedList);
     }
 
     [HttpGet("byName")]
@@ -50,8 +56,12 @@ public sealed class ProductController : ControllerBase
         {
             return Task.FromResult<IActionResult>(BadRequest("Digite um valor válido para realizar a filtragem."));
         }
-        var filteredProductsByName = _productRepository
-            .GetProductsByName(productName.ToString());
+        
+        var filteredProductsByName = _context.Set<Product>()
+            .AsNoTracking()
+            .Where(p => p.Name.ToLower().Trim()
+                .Contains(productName.ToString().ToLower().Trim()))
+            .ToListAsync().Result;
                 
         var totalCount = filteredProductsByName.Count;
         var totalPages = (int)Math.Ceiling(totalCount / (double)PageSize);
@@ -61,12 +71,42 @@ public sealed class ProductController : ControllerBase
         _logger.LogInformation("Produtos filtrados por nome.");
         return Task.FromResult<IActionResult>(Ok(filteredProductsByName));
     }
+    
+    
+    [NonAction]
+    private List<Product> GetProductsByOrderingValue(string orderingValue, int pageSize, int pageIndex )
+    {
+        var products = new List<Product>();
+        
+        var query = _context.Set<Product>();
+        
+        if (orderingValue == Enum.GetName(OrderBy.HighestValue))
+        {
+            products = query!
+                .OrderByDescending(p => p.Price)
+                .AsNoTracking()
+                .Skip((pageIndex - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync().Result;
+        }
+        else if (orderingValue == Enum.GetName(OrderBy.LowerValue))
+        {
+            products = query!
+                .OrderBy(p => p.Price)
+                .AsNoTracking()
+                .Skip((pageIndex - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync().Result;
+        }
+
+        return products;
+    }
 
     [HttpGet("orderBy")]
     public async Task<IActionResult> GetProductsByPrice(int pageIndex)
     {
         
-        var totalCount = _productRepository.GetAllProducts().Result.Count;
+        var totalCount = _context.Set<Product>().ToList().Count;
         var totalPages = (int)Math.Ceiling(totalCount / (double)PageSize);
         HttpContext.Response.Headers.Add("totalPages",totalPages.ToString());
         HttpContext.Response.Headers.Add("totalCount",totalCount.ToString());
@@ -81,16 +121,21 @@ public sealed class ProductController : ControllerBase
                     );
         }
 
-        return Ok(_productRepository
-                .GetProductsByOrderingValue(orderingValue.ToString(), PageSize, pageIndex));
+        return Ok(GetProductsByOrderingValue(orderingValue.ToString(), PageSize, pageIndex));
 
 
+    }
+    [NonAction]
+    private Task<bool> CheckNameExistence(string name)
+    {
+        return Task.FromResult(_context.Set<Product>()!
+            .AnyAsync(p => p.Name.ToLower().Trim() == name.ToLower().Trim()).Result);
     }
     
     [HttpPost]
     public IActionResult Post(ProductModel productModel)
     {
-        if (_productRepository.CheckNameExistence(productModel.Name).Result)
+        if (CheckNameExistence(productModel.Name).Result)
         {
             _logger.LogError("O produto já existe.");
             return BadRequest($"O produto '{productModel.Name}' já existe, por favor digite um nome válido.");
@@ -108,7 +153,7 @@ public sealed class ProductController : ControllerBase
             CreatedAt = DateTimeOffset.Now.ToUniversalTime()
         };
         
-        _productRepository.AddAsync(product);
+        _context.Set<Product>().AddAsync(product);
         _logger.LogInformation("Produto criado com sucesso.");
         return CreatedAtAction(nameof(Get), new { id = product.Id }, productModel);
     }
